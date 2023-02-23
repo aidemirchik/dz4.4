f2b6db41121d190277efff3308e5b1244d37036e
aidemirchik <awirumy14@gmail.com>
2023-02-23 15:52:28 +0600

    dz4.4

--- src/components/person-details/person-details.js
+++ src/components/person-details/person-details.js
@@ -1,30 +1,60 @@
 import React, { Component } from 'react';
 import './person-details.css';
+import SwapiServiceq from '../../services/swapi-service';
+
 
 export default class PersonDetails extends Component {
+  swapiq = new SwapiServiceq()
 
-
+  state = {data:{}}
+  
+  _updatePlanet = (id) => {
+    this.swapiq.getPlanet(id)
+    .then(data => {
+      this.setState({data: data})
+    })
+  }
+  
+  
+  componentDidMount = () => {
+    setInterval (() => {
+  
+    const id = Math.floor(Math.random() * (20 - 1 + 1) + 1)
+    this._updatePlanet(id) 
+    },3000  )
+  }
+  
+  
+  
+      // this.swapi.getPlanet(3)
+      // .then(data => {
+      //   this.setState({data: data})
+      // })
+  
+  
+  
   render() {
+    const {id, name, gender, birthYear, eyeColor} = this.state.data
+    const wwlanetImg =  `https://starwars-visualguide.com/assets/img/characters/${id}.jpg`
 
     return (
       <div className="person-details card">
         <img className="person-image"
-          src='https://starwars-visualguide.com/assets/img/characters/3.jpg'/>
-
+             src={wwlanetImg}/>
         <div className="card-body">
-          <h4>R2-D2</h4>
+          <h4>{name}</h4>
           <ul className="list-group list-group-flush">
             <li className="list-group-item">
               <span className="term">Gender</span>
-              <span>male</span>
+              <span>{gender}</span>
             </li>
             <li className="list-group-item">
               <span className="term">Birth Year</span>
-              <span>43</span>
+              <span>{birthYear}</span>
             </li>
             <li className="list-group-item">
               <span className="term">Eye Color</span>
-              <span>red</span>
+              <span>{eyeColor}</span>
             </li>
           </ul>
         </div>
